5e6e3c6890b4d531bf9104da902722cb4e05f447
ayca494 <aycaturkmen46@gmail.com>
2023-01-01 21:28:24 +0300

    Response

---
+++ APIAndJWT/Domain/Responses/ProductListResponse.cs
@@ -0,0 +1,24 @@
+﻿using APIAndJWT.Domain.Models;
+
+namespace APIAndJWT.Domain.Responses
+{
+    public class ProductListResponse : BaseResponse
+    {
+        public IEnumerable<Product> productList { get; set; }
+        public ProductListResponse(bool success, string message,IEnumerable<Product> productList) : base(success, message)
+        {
+            this.productList= productList;
+        }
+        //başarılı olursa 
+        public ProductListResponse(IEnumerable<Product> productList):this(true,string.Empty,productList)
+        {
+
+        }
+
+        //başarısız olursa
+        public ProductListResponse(string message):this(false,message,null)
+        {
+
+        }
+    }
+}
---
+++ APIAndJWT/Domain/Responses/ProductResponse.cs
@@ -0,0 +1,20 @@
+﻿using APIAndJWT.Domain.Models;
+
+namespace APIAndJWT.Domain.Responses
+{
+    public class ProductResponse : BaseResponse
+    {
+        public Product Product { get; set; }
+        public ProductResponse(bool success, string message,Product product) : base(success, message)
+        {
+            this.Product = product;
+        }
+
+        //başarılı olursa 
+        public ProductResponse(Product product) : this(true, string.Empty, product) { }
+
+        //başarısız olursa
+        public ProductResponse(string message) : this(false, message, null) { }
+        
+    }
+}
