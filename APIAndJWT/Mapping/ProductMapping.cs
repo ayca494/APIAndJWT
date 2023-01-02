88b00a4be65d26b04e5a627c9d087b07a378b738
ayca494 <aycaturkmen46@gmail.com>
2023-01-02 10:04:25 +0300

    Mappee

---
+++ APIAndJWT/Mapping/ProductMapping.cs
@@ -0,0 +1,15 @@
+﻿using APIAndJWT.Domain.Models;
+using APIAndJWT.Resources;
+using AutoMapper;
+namespace APIAndJWT.Mapping
+{
+    public class ProductMapping : Profile
+    {
+        public ProductMapping()
+        {
+            CreateMap<ProductResource, Product>();
+            CreateMap<Product, ProductResource>();
+
+        }
+    }
+}
