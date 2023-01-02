e72020e02d09a407324b2ee448d457bbac182ece
ayca494 <aycaturkmen46@gmail.com>
2023-01-02 10:04:43 +0300

    Resources

---
+++ APIAndJWT/Resources/ProductResource.cs
@@ -0,0 +1,9 @@
+﻿namespace APIAndJWT.Resources
+{
+    public class ProductResource
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Price { get; set; }
+    }
+}
