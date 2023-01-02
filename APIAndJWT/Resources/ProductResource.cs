52a84bce784bf60cb9bca41a0576d78c1abc97a4
ayca494 <aycaturkmen46@gmail.com>
2023-01-02 15:16:29 +0300

    Required added to ProductResource

--- APIAndJWT/Resources/ProductResource.cs
+++ APIAndJWT/Resources/ProductResource.cs
@@ -1,9 +1,14 @@
-﻿namespace APIAndJWT.Resources
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIAndJWT.Resources
 {
     public class ProductResource
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Category { get; set; }
-        public string Price { get; set; }
+        [Required]
+        public decimal Price { get; set; }
     }
 }
