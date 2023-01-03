a4f0a5f5fb65df27ab412c614b13a9887d155df8
ayca494 <aycaturkmen46@gmail.com>
2023-01-03 09:04:26 +0300

    Services and Middleware

--- APIAndJWT/Program.cs
+++ APIAndJWT/Program.cs
@@ -1,4 +1,9 @@
 using APIAndJWT.Domain.Entities;
+using APIAndJWT.Domain.Repositories;
+using APIAndJWT.Domain.Services;
+using APIAndJWT.Domain.UnitOfWork;
+using APIAndJWT.Services;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +14,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.AddTransient<IProductService, ProductService>(); //her karþýlaþtýðýnda nesne oluþturur.
+builder.Services.AddScoped<IProductService, ProductService>(); //bir kere nesne oluþtutur bir request boyunca her karþýlaþtýðýnda onu kullanýr.
+//builder.Services.AddSingleton<IProductService, ProductService>(); //bir kere nesne oluþtutur bir request boyunca ve uygulamanýn yaþam döngüsü býyunca her karþýlaþtýðýnda onu kullanýr.
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+builder.Services.AddCors(opts =>
+{
+    opts.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); //izinler veriliyor.
+    });
+
+    ////Sadece bu siteden gelen isteklere cevap verilmesi
+    //opts.AddPolicy("abcPolicy", builder =>
+    //{
+    //    builder.WithOrigins("https://www.abc.com").AllowAnyHeader().AllowAnyMethod();
+    //});
+});
 
 builder.Services.AddDbContext<UdemyAPIContext>(options =>
 {
@@ -29,5 +57,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//app.UseCors("abcPolicy"); //sadece istediðimiz bir siteye izin verirsek middleware bu þekilde ekliyoruz.
+
+
+app.UseCors();
 
 app.Run();
